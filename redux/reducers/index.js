e5af36c56cc077e0127760b6760c5353e4847d41
nicholsen-guoziyu <nicholsen.norman@hotmail.com>
2020-04-25 00:50:40 +1200

    react redux

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,5 +1,3 @@
-// src/js/reducers/index.js
-
 import { ADD_ARTICLE } from "../constants/action-types";
 
 const initialState = {
@@ -8,7 +6,9 @@ const initialState = {
 
 function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
-    state.articles.push(action.payload);
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
   }
   return state;
 }
---
+++ webpack.client.react.redux.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const path = require ('path');
+
+const APP_DIR = path.resolve(__dirname,'redux');
+const PUBLIC_DIR = path.resolve(__dirname,'public');
+
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+
+const config = {
+
+    entry: APP_DIR + '/react-redux-index.js',
+    devServer:{
+        contentBase: PUBLIC_DIR,
+        port: 9000,
+        open: true,
+        historyApiFallback: true
+    },
+    output: {
+        path: PUBLIC_DIR,
+        filename: 'clientreactreduxbundle.js'
+    }
+
+};
+
+const x = merge(baseConfig, config);
+console.log(x);
+
+//module.exports = config;
+module.exports = merge(baseConfig, config);
