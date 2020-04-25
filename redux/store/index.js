e020392ef19eb91fb4af6e732cd60fb3b59f9f1f
nicholsen-guoziyu <nicholsen.norman@hotmail.com>
2020-04-25 13:14:00 +1200

    redux dev tools for debugging

--- redux/store/index.js
+++ redux/store/index.js
@@ -1,6 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
+import { forbiddenWordsMiddleware } from "../middleware";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+    composeWithDevTools(applyMiddleware(forbiddenWordsMiddleware))
+);
 
 export default store;
