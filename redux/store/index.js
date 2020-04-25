86565126ed7ac67c3fb3e611bd5cccf49d6a6735
nicholsen-guoziyu <nicholsen.norman@hotmail.com>
2020-04-25 16:50:27 +1200

    redux-thunk

--- redux/store/index.js
+++ redux/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
+import thunk from "redux-thunk";
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(rootReducer,
-    composeWithDevTools(applyMiddleware(forbiddenWordsMiddleware))
+    composeWithDevTools(applyMiddleware(forbiddenWordsMiddleware, thunk))
 );
 
 export default store;
